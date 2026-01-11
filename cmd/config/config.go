740580ce8ea52972dcf3dffcfdffbf8dc28cf774
mpapenbr <Markus.Papenbrock+github@gmail.com>
2026-01-11 22:55:41 +0100

    Add connection pool to DB Fixes #126

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,5 +1,27 @@
 package config
 
+type (
+	DBConfig struct {
+		Enabled       bool
+		Host          string
+		Port          int
+		Database      string
+		StaticSecrets DBSecrets // static secrets defined in config
+		SecretsFile   string    // path to file with secrets (e.g., from Vault)
+		SSLMode       string
+		TLSCert       string // path to TLS certificate
+		TLSKey        string // path to TLS key
+		TLSCA         string // path to TLS CA (to validate server certificate)
+	}
+	// this holds secrets for the DB that are managed outside of the main config
+
+	DBSecrets struct {
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		// TODO: mTLS settings
+	}
+)
+
 var (
 	EnableTelemetry   bool
 	TelemetryEndpoint string
@@ -15,4 +37,5 @@ var (
 	TLSClientAuth     string   // TLS client authentication mode
 	Address           string   // address to listen on/connect to
 	OtelOutput        string   // output for otel-logger (stdout, grpc)
+	DBConf            DBConfig
 )
